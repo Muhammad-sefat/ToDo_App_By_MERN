e7e2aa1080ce0aa1ffbd0e45f8eb37175fc21a45
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-02-25 23:31:59 +0600

    Connect with mongoDB and create login and register controller

---
+++ backend/controller/authController.js
@@ -0,0 +1,42 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    let user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ msg: "User already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    user = new User({ name, email, password: hashedPassword });
+
+    await user.save();
+    res.status(201).json({ msg: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -11,5 +11,10 @@ app.use(cors());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 
+app.get("/", (req, res) => {
+  res.send("ToDo_App Backend is Running...");
+});
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
