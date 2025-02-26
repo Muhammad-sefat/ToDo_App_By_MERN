ab70018eeeac48f5167a149221d531255f351ea3
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-02-26 18:10:30 +0600

    Create task sechema in backend

---
+++ backend/models/task.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: { type: String, required: true },
+    description: { type: String },
+    dueDate: { type: Date, required: true },
+    status: {
+      type: String,
+      enum: ["Pending", "Completed"],
+      default: "Pending",
+    },
+    priority: { type: String, enum: ["High", "Medium", "Low"], required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
