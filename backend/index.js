d3a02c392e9f3512a97505817a7b62282551a7c6
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-03-07 10:20:23 +0600

    Try to fix deployment issue

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: ["http://localhost:5173"],
   credentials: true,
-  optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 app.use(express.json());
