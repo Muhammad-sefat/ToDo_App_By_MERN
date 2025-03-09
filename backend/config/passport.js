4df2c9be30db50e105fc568df8d27523749aae36
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-03-09 09:01:10 +0600

    Final Commit

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -2,12 +2,17 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/userModel");
 
+const callbackURL =
+  process.env.NODE_ENV === "production"
+    ? "https://todoapp-backend-kappa.vercel.app/api/auth/google/callback"
+    : "http://localhost:5000/api/auth/google/callback";
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/auth/google/callback",
+      callbackURL,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
